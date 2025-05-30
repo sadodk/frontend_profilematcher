a9f68c5399f8fb3c8b7bf66a3bd8f125040d1dd1
Saad Afzal <saaddk@gmail.com>
2025-05-30 19:02:08 +0200

    cleaner coded image picker

--- src/app/(tabs)/new.tsx
+++ src/app/(tabs)/new.tsx
@@ -1,12 +1,4 @@
-import { Link } from 'expo-router';
-import {
-	StyleSheet,
-	Text,
-	View,
-	Image,
-	TextInput,
-	Pressable,
-} from 'react-native';
+import { Text, View, Image, TextInput, Pressable } from 'react-native';
 import { useEffect, useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
 
@@ -14,52 +6,44 @@ export default function CreatePost() {
 	const [caption, setCaption] = useState('');
 	const [image, setImage] = useState<string | null>(null);
 
-	useEffect(() => {
-		if (!image) {
-			pickImage();
-		}
-	}, [image]);
-
 	const pickImage = async () => {
-		// No permissions request is necessary for launching the image library
-		let result = await ImagePicker.launchImageLibraryAsync({
+		const result = await ImagePicker.launchImageLibraryAsync({
 			mediaTypes: ['images'],
 			allowsEditing: true,
 			aspect: [4, 3],
 			quality: 1,
 		});
 
-		console.log(result);
-
 		if (!result.canceled) {
 			setImage(result.assets[0].uri);
 		}
 	};
 
+	useEffect(() => {
+		if (!image) pickImage();
+	}, [image]);
+
+	const ImageComponent = image ? (
+		<Image
+			source={{ uri: image }}
+			className="w-52 aspect-[3/4] rounded-lg mb-4 shadow-md"
+		/>
+	) : (
+		<View className="w-52 aspect-[3/4] rounded-lg mb-4 bg-gray-200 items-center justify-center" />
+	);
+
 	return (
 		<View className="p-3 items-center flex-1 bg-white">
-			{/* Image Picker */}
-			{image ? (
-				<Image
-					source={{
-						uri: image,
-					}}
-					className="w-52 aspect-[3/4] rounded-lg mb-4 shadow-md"
-				/>
-			) : (
-				<View className="w-52 aspect-[3/4] rounded-lg mb-4 bg-gray-200 items-center justify-center" />
-			)}
+			{ImageComponent}
 			<Text onPress={pickImage} className="text-blue-500 font-semibold m-5">
 				Change
 			</Text>
-			{/* Text Input for caption */}
 			<TextInput
 				value={caption}
-				onChangeText={(newValue) => setCaption(newValue)}
+				onChangeText={setCaption}
 				placeholder="Write a caption..."
 				className="border border-gray-300 rounded-md p-3 w-full"
 			/>
-			{/* Button to create post */}
 			<View className="w-full mt-auto">
 				<Pressable
 					className="bg-blue-500 rounded-md p-3 w-full mt-5"
